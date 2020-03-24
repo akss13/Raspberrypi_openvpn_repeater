24b444f4a3cef27c4c3769728246cf703596b5d5
akss13 <aakarsh.s.saini@gmail.com>
2020-03-24 10:26:35 -0400

    minor changes

--- automatescript.sh
+++ automatescript.sh
@@ -11,9 +11,9 @@ function driver() {
     if [ $(id -u) != 0 ]; then
         echo "Run the script as root"
     elif [ -z "$var" ]; then
-        echo "enter client file name"
+        echo "enter client file absoulute path"
     else
-        autossh -D 8008 root@162.243.166.99 -p 443
+        autossh -D 8008 root@162.243.166.99 -p 443 & 
         sudo openvpn "$var" &
         sudo mv /etc/resolv.conf /etc/resolv.conf.orig
         echo "nameserver 8.8.8.8">/etc/resolv.conf
